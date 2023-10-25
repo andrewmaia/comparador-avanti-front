a168b1bf10abcca80ef7532158224ec01288a3cb
Andrew Maia <andrewsmaia@gmail.com>
2023-10-25 17:05:14 -0300

    Pre selecao de setor e carregamento de mais partidas

--- src/app.js
+++ src/app.js
@@ -3,13 +3,18 @@
 "use strict";
 
 const backendDefaultUrl =
-  "https://nreq2pfx04.execute-api.us-east-1.amazonaws.com/Prod";
+  "https://hse1s4vpq3.execute-api.us-east-1.amazonaws.com/Prod";
 
 // Selectors
 const h2Resultado = document.getElementById("h2Resultado");
 const secResultado = document.getElementById("secResultado");
 const btnComparar = document.getElementById("btnComparar");
 const rodape = document.getElementsByTagName("footer")[0];
+const ulJogos = document.getElementById("ulJogos");
+
+let podeCarregarMaisJogo,
+  adicionouBotaoCarregarMaisJogos = false,
+  jogoLastEvaluatedKey;
 
 // Event Listeners
 btnComparar.addEventListener("click", comparar);
@@ -52,55 +57,81 @@ function carregarJogos() {
   const xhttp = new XMLHttpRequest();
   xhttp.onload = mostrarJogos;
 
-  xhttp.open("GET", `${backendDefaultUrl}/jogos`);
-  xhttp.send();
+  xhttp.open("POST", `${backendDefaultUrl}/jogos`);
+  xhttp.send(JSON.stringify(jogoLastEvaluatedKey));
 }
 
 function mostrarJogos() {
-  const jogos = JSON.parse(this.responseText);
-  let jogosHtml = "";
+  const retorno = JSON.parse(this.responseText);
+  retorno.Items.forEach(montarSelectJogo);
+  jogoLastEvaluatedKey = retorno.LastEvaluatedKey;
+  exibirBotaoCarregarMaisJogos();
+}
 
-  jogos.forEach((jogo) => {
-    jogosHtml =
-      jogosHtml +
-      `<li class="jogo">
-      <article>
-        <h3>Palmeiras x ${jogo.adversario}</h3>
-        <h4>${new Date(jogo.dataJogo).toLocaleDateString("pt-BR", {
-          timeZone: "UTC",
-        })}</h4>
-        <label for="setor">Setor:</>
-        <select id="jogo_${jogo.id}" name="${jogo.id}">
-          <option value="">Não fui</option>        
-          <option value="sn">Superior Norte: ${formatarDinheiro(
-            jogo.superiorNorteValor
-          )}</option>
-          <option value="ss">Superior Sul: ${formatarDinheiro(
-            jogo.superiorSulValor
-          )}</option>
-          <option value="so">Superior Oeste: ${formatarDinheiro(
-            jogo.superiorOesteValor
-          )}</option>
-          <option value="sl">Superior Leste: ${formatarDinheiro(
-            jogo.superiorLesteValor
-          )}</option>          
-          <option value="gn">Gol Norte: ${formatarDinheiro(
-            jogo.golNorteValor
-          )}</option>
-          <option value="gs">Gol Sul: ${formatarDinheiro(
-            jogo.golSulValor
-          )}</option>
-          <option value="cl">Central Leste: ${formatarDinheiro(
-            jogo.centralLesteValor
-          )}</option>
-          <option value="co">Central Oeste: ${formatarDinheiro(
-            jogo.centralOesteValor
-          )}</option>          
-        </select>        
-      </article>
-    </li>`;
-  });
-  document.getElementById("ulJogos").innerHTML = jogosHtml;
+function montarSelectJogo(jogo) {
+  const li = document.createElement("li");
+  li.setAttribute("class", "jogo");
+  li.innerHTML = `<article>
+      <h3>Palmeiras x ${jogo.adversario}</h3>
+      <h4>${new Date(jogo.dataJogo).toLocaleDateString("pt-BR", {
+        timeZone: "UTC",
+      })}</h4>
+      <label for="setor">Setor:</>
+      <select id="jogo_${jogo.id}" name="${jogo.id}"
+        onchange="salvarSetor(this)">
+        <option value="">Não fui</option>        
+        <option value="sn" ${preSelecionado(jogo.id, "sn")}>
+          Superior Norte: ${formatarDinheiro(jogo.superiorNorteValor)}
+        </option>
+        <option value="ss" ${preSelecionado(jogo.id, "ss")}>
+          Superior Sul: ${formatarDinheiro(jogo.superiorSulValor)}
+        </option>
+        <option value="so" ${preSelecionado(jogo.id, "so")}>
+          Superior Oeste: ${formatarDinheiro(jogo.superiorOesteValor)}
+        </option>
+        <option value="sl" ${preSelecionado(jogo.id, "sl")}>
+          Superior Leste: ${formatarDinheiro(jogo.superiorLesteValor)}
+        </option>          
+        <option value="gn" ${preSelecionado(jogo.id, "gn")}>
+          Gol Norte: ${formatarDinheiro(jogo.golNorteValor)}
+        </option>
+        <option value="gs" ${preSelecionado(jogo.id, "gs")}>
+          Gol Sul: ${formatarDinheiro(jogo.golSulValor)}
+        </option>
+        <option value="cl" ${preSelecionado(jogo.id, "cl")}>
+          Central Leste: ${formatarDinheiro(jogo.centralLesteValor)}
+        </option>
+        <option value="co" ${preSelecionado(jogo.id, "co")}>
+          Central Oeste: ${formatarDinheiro(jogo.centralOesteValor)}
+        </option>          
+      </select>        
+    </article>`;
+
+  if (adicionouBotaoCarregarMaisJogos)
+    ulJogos.insertBefore(li, ulJogos.lastChild);
+  else ulJogos.appendChild(li);
+}
+function exibirBotaoCarregarMaisJogos() {
+  podeCarregarMaisJogo = jogoLastEvaluatedKey !== undefined;
+  if (podeCarregarMaisJogo) {
+    if (!adicionouBotaoCarregarMaisJogos) {
+      adicionouBotaoCarregarMaisJogos = true;
+      const li = document.createElement("li");
+      li.setAttribute("class", "maisJogos");
+      const article = document.createElement("article");
+      const h3 = document.createElement("h3");
+      const aMaisJogos = document.createElement("a");
+      aMaisJogos.innerText = "Carregar mais";
+      aMaisJogos.addEventListener("click", carregarJogos);
+      h3.appendChild(aMaisJogos);
+      article.appendChild(h3);
+      li.appendChild(article);
+      ulJogos.appendChild(li);
+    }
+  } else {
+    const li = document.getElementsByClassName("maisJogos")[0];
+    li.remove();
+  }
 }
 
 function carregarRodape() {
@@ -172,4 +203,15 @@ function mostrarComparacao() {
 //Auxiliares
 function formatarDinheiro(valor) {
   return "R$ " + valor.toLocaleString(undefined, { minimumFractionDigits: 2 });
+}
+
+function preSelecionado(jogoId, setor) {
+  let setorSelecionado = localStorage.getItem(jogoId);
+  if (setorSelecionado !== undefined && setor === setorSelecionado) {
+    return "selected";
+  }
+  return "";
+}
+function salvarSetor(select) {
+  localStorage.setItem(select.name, select.value);
 }
