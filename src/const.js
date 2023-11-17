1a1a03965d94322ebb5fbf7658828dae1ab3715e
Andrew Maia <andrewsmaia@gmail.com>
2023-11-17 08:59:05 -0300

    API

--- src/const.js
+++ src/const.js
@@ -2,4 +2,4 @@
 
 // eslint-disable-next-line no-unused-vars
 const backendDefaultUrl =
-  "https://7iami9ukji.execute-api.us-east-1.amazonaws.com/Prod";
+  "https://yta51hlc75.execute-api.us-east-1.amazonaws.com/Prod";
